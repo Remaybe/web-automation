9d067376b42eb77926b1aedf2fc239cbf803cabe
Maznev Egor <remaybe23@gmail.com>
2021-03-10 15:42:20 +0200

    * added testng in pom.xml * fixed driver paths

--- src/test/java/Browsers.java
+++ src/test/java/Browsers.java
@@ -3,15 +3,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public enum Browsers {
+
     FIREFOX {
         public WebDriver create() {
-            System.setProperty("webdriver.gecko.driver", "/Users/emaznev/Downloads/geckodriver");
+            System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/geckodriver");
             return new FirefoxDriver();
         }
     },
     CHROME {
         public WebDriver create() {
-            System.setProperty("webdriver.chrome.driver", "/Users/emaznev/Downloads/chromedriver-2");
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver-2");
             return new ChromeDriver();
         }
     };
