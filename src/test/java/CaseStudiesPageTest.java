de234c4d8e9ea44c80e7ab644fa60f00385df6b6
Maznev Egor <remaybe23@gmail.com>
2021-03-15 11:00:12 +0200

    BaseTest class added

--- src/test/java/CaseStudiesPageTest.java
+++ src/test/java/CaseStudiesPageTest.java
@@ -1,31 +1,11 @@
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class CaseStudiesPageTest {
-    private WebDriver driver;
+public class CaseStudiesPageTest extends BaseTest {
     private CaseStudiesPage caseStudies;
-
-    @BeforeTest
-    public void setUp(){
-        driver = ChromeBrowser.createBrowser();
-        ChromeBrowser.setProp(driver);
-        driver.get("https://csma-staging.griddynamics.net/projects/364");
-        AuthPage authPage = new AuthPage(driver);
-        authPage.auth("emaznev", "@85411321eGo885441113221");
-        caseStudies = new CaseStudiesPage(driver);
-    }
 
     @Test
     public void firstTest(){
 
     }
 
-    @AfterTest
-    public void tearDown(){
-        driver.quit();
-    }
 }
--- src/test/java/ProjectsPageTest.java
+++ src/test/java/ProjectsPageTest.java
@@ -1,42 +1,21 @@
-import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import com.google.common.base.Verify;
 import org.testng.annotations.Test;
 
-public class ProjectsPageTest {
-
-    private WebDriver driver;
-    private ProjectsPage projectsPage;
-
-    @BeforeTest
-    public void setUp(){
-        driver = ChromeBrowser.createBrowser();
-        ChromeBrowser.setProp(driver);
-        driver.get("https://csma-staging.griddynamics.net/projects");
-        AuthPage authPage = new AuthPage(driver);
-        authPage.auth("emaznev", "@85411321eGo885441113221");
-        projectsPage = new ProjectsPage(driver);
-    }
+public class ProjectsPageTest extends BaseTest {
 
     @Test
     public void searchUsingCmbbxs(){
-        projectsPage.filterByCmbbxValue(Comboboxes.ACCOUNTS, "Raymond James")
+        projectsPage
+                .filterByCmbbxValue(Comboboxes.ACCOUNTS, "Raymond James")
                 .filterByCmbbxValue(Comboboxes.PROJECTS, "106 BOM - Trade Corrections CO2")
                 .filterByCmbbxValue(Comboboxes.AREAS, "Fraud Protection")
                 .filterByCmbbxValue(Comboboxes.TECHNOLOGIES, "Azure");
-        Assert.assertEquals(projectsPage.getProjectHeader(),
-                "106 BOM - Trade Corrections CO2");
+        Verify.verify(projectsPage.getProjectHeader().equals("106 BOM - Trade Corrections CO2"));
     }
 
     @Test
     public void clrButtonStatusWithoutValues(){
-        Assert.assertEquals(projectsPage.chckButtonStatus(), false);
-    }
-
-    @AfterTest
-    public void tearDown(){
-        driver.quit();
+        Verify.verify(projectsPage.chckButtonStatus(), "Button should be non-clickable");
     }
 
 }
